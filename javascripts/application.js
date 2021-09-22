e832d3e13338904d61713b1aab6cf0060f77f5b0
Robert Pihelgas <robertpihelgas@gmail.com>
2021-09-22 16:08:11 +0300

    Add JS for ecommerce layouts

--- javascripts/application.js
+++ javascripts/application.js
@@ -775,22 +775,93 @@ MMCQ = (function() {
 
   var editmode = $('html').hasClass('editmode');
 
-  // Remove comments if debouncing is used.
   // Function to limit the rate at which a function can fire.
-  // var debounce = function(func, wait, immediate) {
-  //   var timeout;
-  //   return function() {
-  //     var context = this, args = arguments;
-  //     var later = function() {
-  //       timeout = null;
-  //       if (!immediate) func.apply(context, args);
-  //     };
-  //     var callNow = immediate && !timeout;
-  //     clearTimeout(timeout);
-  //     timeout = setTimeout(later, wait);
-  //     if (callNow) func.apply(context, args);
-  //   };
-  // };
+  var debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  var bindSideClicks = function() {
+    $('.content').on('mousedown', function(event) {
+      if (!$(event.target).closest('.js-prevent-sideclick').length) {
+        $('.js-image-settings-popover').toggleClass('active');
+      };
+    });
+  };
+
+// ===========================================================================
+  // Load product cover images only when they are close or appearing in the
+  // viewport.
+  // ===========================================================================
+  var callback_loaded = function (element) {
+    var $contentItemBox = $(element).closest('.js-content-item-box');
+    $contentItemBox.removeClass('not-loaded with-error').addClass('is-loaded');
+  };
+
+  var bindContentItemImageLazyload = new LazyLoad({
+    threshold: 500,
+    elements_selector: ".js-lazyload",
+    callback_loaded: callback_loaded
+  });
+
+  $('.js-cart-btn').click(function() {
+    if ($(this).data('product-id')) {
+      Voog.ShoppingCart.addProductById($(this).data('product-id'))
+    }
+  });
+
+  var handleWindowScroll = function() {
+    window.addEventListener('scroll', function(e) {
+      var wrapperHeight = $('.header_fixed').height();
+      $('.t-sticky').css('top', $('.header_fixed').outerHeight() + 32);
+
+      if (window.scrollY > wrapperHeight) {
+        $('.header_fixed:not(.relative)').addClass('scroll');
+        $('body').addClass('scroll');
+      } else {
+        $('.header_fixed').removeClass('scroll');
+        $('body').removeClass('scroll');
+      }
+    });
+  };
+
+  var handleProductPageContent = function() {
+    $(document).ready(function() {
+      changeProductImagePos();
+    });
+
+    $(window).resize(debounce(function() {
+      changeProductImagePos();
+    }, 25));
+
+    var changeProductImagePos = function() {
+      var paroductImage = $('.js-product-page-image');
+      var paroductImageWrap = $('.js-product-page-image-wrap');
+      var buyBtnContent = $('.js-buy-btn-content');
+
+      if ($('.js-buy-btn-content .edy-buy-button-container').length >= 1) {
+        if ($( window ).width() <= 752) {
+          if ($('.js-buy-btn-content .js-product-page-image').length <= 0) {
+            buyBtnContent.prepend(paroductImage);
+          }
+        } else {
+          if ($('.js-product-page-image-wrap .js-product-page-image').length <= 0) {
+            paroductImageWrap.prepend(paroductImage);
+          }
+        }
+      }
+    }
+  }
 
   var bindFallbackHeaderLeftWidthCalculation = function() {
     var headerWidth = $('.js-header-inner').width(),
@@ -1202,6 +1273,7 @@ MMCQ = (function() {
 };
 
 var init = function() {
+  bindSideClicks();
   toggleMainMenu();
   toggleLangMenu();
   handlePopoverMenuHide();
@@ -1209,6 +1281,7 @@ var init = function() {
   handleSearch();
   wrapTables();
   focusCommentsWithErrors();
+  handleWindowScroll();
 
   $('.content form').edicyFormPlaceholders();
 
@@ -1222,7 +1295,8 @@ var init = function() {
     initCommonPage: initCommonPage,
     initBlogPage: initBlogPage,
     initArticlePage: initArticlePage,
-    bindSiteSearch: bindSiteSearch
+    bindSiteSearch: bindSiteSearch,
+    handleProductPageContent:handleProductPageContent
   });
 
 init();
